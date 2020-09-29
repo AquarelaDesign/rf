64988f8b961b7a81f16302a07f5d4c456c1f5a84
Jose Augusto Freire <aquarela.design@gmail.com>
2020-09-29 17:30:17 -0300

    Ajustes nas Rotas

--- rf-dashboard/src/components/Map.js
+++ rf-dashboard/src/components/Map.js
@@ -1,12 +1,35 @@
 import React, {useState, useEffect, useCallback, memo} from 'react'
-import { GoogleMap, LoadScript, Marker, DirectionsService, DirectionsRenderer,DistanceMatrixService } from '@react-google-maps/api'
+
+import { 
+  GoogleMap, 
+  LoadScript, 
+  Marker, 
+  DirectionsService, 
+  DirectionsRenderer,
+  DistanceMatrixService, 
+  InfoWindow,
+  Polyline,
+} from '@react-google-maps/api'
+
 import { v4 as uuidv4 } from 'uuid'
- 
+import { makeStyles } from '@material-ui/core/styles'
+
 const containerStyle = {
   width: '100%',
-  height: '100%'
+  height: '90%'
 }
  
+const useStyles = makeStyles((theme) => ({
+  infobox: {
+    position: 'absolute',
+    top: 110,
+    right: 32,
+    width: '150px',
+    height: '50px',
+    border: '1px solid #2699F8'
+  },
+}))
+
 const apiKey = "AIzaSyBoV-kvy8LfddqcUb6kcHvs5TmrRJ09KXY"
 
 function errorHandler(err) {
@@ -17,9 +40,13 @@ function errorHandler(err) {
   }
 }
 
-const options = { timeout: 60000 }
+const options = { 
+  timeout: 60000,
+}
 
 const Map = ({ places, defaultCenter }) => {
+  const classes = useStyles()
+  
   const [map, setMap] = useState(null)
   const [center, setCenter] = useState(defaultCenter)
 
@@ -27,9 +54,14 @@ const Map = ({ places, defaultCenter }) => {
   const [travelMode, setTravelMode] = useState('DRIVING')
   const [origin, setOrigin] = useState('')
   const [destination, setDestination] = useState('')
+  
+  const [dadosInfo, setDadosInfo] = useState({
+    distancia: '',
+    tempo: '',
+  })
  
   useEffect(() => {
-    console.log('**** Map.start', center, places)
+    // console.log('**** Map.start', center, places)
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         showLocation,
@@ -39,7 +71,7 @@ const Map = ({ places, defaultCenter }) => {
       
       setTravelMode('DRIVING')
       
-      console.log('**** Maps.places', places)
+      // console.log('**** Maps.places', places)
 
       if (places[0]) {
         setOrigin(places[0])
@@ -57,21 +89,6 @@ const Map = ({ places, defaultCenter }) => {
       lat: position.coords.latitude,
       lng: position.coords.longitude
     })
-  }
-
-  const getOrigin = (ref) => {
-    setOrigin(ref)
-  }
-
-  const getDestination = (ref) => {
-    setDestination(ref)
-  }
-
-  const onClick = () => {
-    if (origin.value !== '' && destination.value !== '') {
-      setOrigin(origin.value)
-      setDestination(destination.value)
-    }
   }
 
   const onMapClick = (...args) => {
@@ -94,6 +111,12 @@ const Map = ({ places, defaultCenter }) => {
     if (resp !== null) {
       if (resp.status === 'OK') {
         setResponse(resp)
+        console.log('**** Maps.directionsCallback', resp.routes[0].legs[0].distance)
+
+        setDadosInfo({
+          distancia: resp.routes[0].legs[0].distance.text,
+          tempo: `${Math.round((resp.routes[0].legs[0].distance.value / 1000) / 500)} dias`,
+        })
       } else {
         console.log('resp: ', resp)
       }
@@ -102,101 +125,108 @@ const Map = ({ places, defaultCenter }) => {
 
   const distanceCallback = (resp) => {
     // console.log('**** Maps.distanceCallback', resp)
-
-    if (resp !== null) {
-      if (resp.status === "OK") {
-        console.log("**** Maps.distanceCallback.origin: ", resp.originAddresses[0])
-        console.log("**** Maps.distanceCallback.destination: ", resp.destinationAddresses[0])
-        console.log("**** Maps.distanceCallback.distance: ", resp.rows[0].elements[0].distance)
-      } else {
-        console.log("**** Maps.distanceCallback.originAddresses: ", resp.originAddresses[0])
-        console.log("**** Maps.distanceCallback.destinationAddresses: ", resp.destinationAddresses[0])
-        console.log("**** Maps.distanceCallback.distance: ", resp.rows[0].elements[0].distance)
-      }
-    }
   }
-  
+
+  const options = {
+    strokeColor: '#FF0000',
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: '#FF0000',
+    fillOpacity: 0.35,
+    clickable: false,
+    draggable: false,
+    editable: false,
+    visible: true,
+    radius: 30000,
+    paths: places,
+    zIndex: 1
+  }
+
   return (
-    <LoadScript
-      googleMapsApiKey={apiKey}
-    >
-      <GoogleMap
-        id='direction-map'
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={2}
-        onLoad={onLoad}
-        onUnmount={onUnmount}
-        onClick={onMapClick}
+    <div style={{ width: '100%', height: '100%' }}>
+      <LoadScript
+        googleMapsApiKey={apiKey}
       >
-        {
-          places.map(maker => (
-            <Marker
-              key={uuidv4()}
-              position={maker}
-            />
-          ))
-        }
-
-        {
-          (
-            destination !== '' && origin !== ''
-          ) && (
-            <>
-              {/* { console.log('**** Maps.origin,destination', origin, destination) } */}
-              <DirectionsService
-                // required
-                options={{ // eslint-disable-line react-perf/jsx-no-new-object-as-prop
-                  destination: destination,
-                  origin: origin,
-                  travelMode: travelMode
-                }}
-                // required
-                callback={directionsCallback}
-                // optional
-                // onLoad={directionsService => {
-                //   console.log('DirectionsService onLoad directionsService: ', directionsService)
-                // }}
-                // optional
-                // onUnmount={directionsService => {
-                //   console.log('DirectionsService onUnmount directionsService: ', directionsService)
-                // }}
+        <GoogleMap
+          id='direction-map'
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={2}
+          onLoad={onLoad}
+          onUnmount={onUnmount}
+          onClick={onMapClick}
+        >
+          {
+            places.map(maker => (
+              <Marker
+                key={uuidv4()}
+                position={maker}
               />
-              <DistanceMatrixService
-                options={{
-                  destinations: [destination],
-                  origins: [origin],
-                  travelMode: travelMode,
-                }}
-                callback={distanceCallback}
-              />
-            </>
+            ))
+          }
+          
+          {/* 
+          <Polyline
+            // onLoad={onLoad}
+            path={places}
+            options={options}
+          /> 
+          */}
 
 
-          )
-        }
+          {
+            (
+              destination !== '' && origin !== ''
+            ) && (
+              <>
+                <DirectionsService
+                  options={{ // eslint-disable-line react-perf/jsx-no-new-object-as-prop
+                    destination: destination,
+                    origin: origin,
+                    travelMode: travelMode
+                  }}
+                  callback={directionsCallback}
+                />
 
-        {
-          response !== null && (
-            <DirectionsRenderer
-              // required
-              options={{ // eslint-disable-line react-perf/jsx-no-new-object-as-prop
-                directions: response
-              }}
-              // optional
-              onLoad={directionsRenderer => {
-                console.log('DirectionsRenderer onLoad directionsRenderer: ', directionsRenderer)
-              }}
-              // optional
-              onUnmount={directionsRenderer => {
-                console.log('DirectionsRenderer onUnmount directionsRenderer: ', directionsRenderer)
-              }}
-            />
-          )
-        }
+                {
+                  response !== null && (<>
+                    <DistanceMatrixService
+                      options={{
+                        destinations: [destination],
+                        origins: [origin],
+                        travelMode: travelMode,
+                      }}
+                      callback={distanceCallback}
+                    />
 
-      </GoogleMap>
-    </LoadScript>
+                    <DirectionsRenderer
+                      options={{ // eslint-disable-line react-perf/jsx-no-new-object-as-prop
+                        directions: response
+                      }}
+                    />
+                  </>)
+                }
+              </>
+            )
+          } 
+
+        </GoogleMap>
+      </LoadScript>
+
+      <div style={{ 
+        // width: '100%', 
+        height: '10%', 
+        fontSize: 16, 
+        color: '#000000', 
+        margin: 'auto',
+        width: '50%',
+        padding: '10px',
+        textAlign: 'center',
+      }}>
+        Distância aproximada: <span style={{ fontWeight: 'bold', marginRight: '30px' }}>{dadosInfo.distancia}</span>
+        Tempo estimado: <span style={{ fontWeight: 'bold' }}>{dadosInfo.tempo}</span>
+      </div> 
+    </div>
   )
 }
  
